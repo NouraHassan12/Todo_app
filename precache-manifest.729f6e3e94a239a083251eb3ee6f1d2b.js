a8a5168bbe2731ede2048673f837ee030d233dc4
Nourhassann <nourhassan132@gmail.com>
2022-06-28 15:19:33 +0200

    Updates

---
+++ precache-manifest.729f6e3e94a239a083251eb3ee6f1d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b73a8a27476bbb5a87d442dc6b43dd8",
+    "url": "/Todo_app/index.html"
+  },
+  {
+    "revision": "17e014dbc972bde70f9c",
+    "url": "/Todo_app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a7bb10fec39bd963f648",
+    "url": "/Todo_app/static/css/main.420e70c2.chunk.css"
+  },
+  {
+    "revision": "17e014dbc972bde70f9c",
+    "url": "/Todo_app/static/js/2.1c2c9aa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo_app/static/js/2.1c2c9aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bb10fec39bd963f648",
+    "url": "/Todo_app/static/js/main.95aa7dd2.chunk.js"
+  },
+  {
+    "revision": "f86ee39472dcb751ab69",
+    "url": "/Todo_app/static/js/runtime-main.2daffff6.js"
+  }
+]);
